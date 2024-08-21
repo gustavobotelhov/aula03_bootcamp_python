0d3900693f3c9d7d82d5d85ca312505350844662
Gustavo Botelho Vieira <gu-botelho@hotmail.com>
2024-08-21 09:40:13 -0300

    adicionando exercicio 7

--- exercicios.py
+++ exercicios.py
@@ -62,12 +62,12 @@
 # a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
 # Dada uma transação como `transacao = {'valor': 12000, 'hora': 20}`, verifique se ela é suspeita.
 
-transacao = {'valor': 12000, 'hora': 20}
+# transacao = {'valor': 12000, 'hora': 20}
 
-if transacao['valor'] > 10000 or transacao['hora'] < 9 or transacao['hora'] > 18:
-    print("Transação suspeita")
-else:
-    print("Transação normal")
+# if transacao['valor'] > 10000 or transacao['hora'] < 9 or transacao['hora'] > 18:
+#     print("Transação suspeita")
+# else:
+#     print("Transação normal")
 
 
 ### Exercício 6. Contagem de Palavras em Textos
@@ -94,6 +94,13 @@ else:
 
 ### Exercício 7. Normalização de Dados
 # Objetivo:** Normalizar uma lista de números para que fiquem na escala de 0 a 1.
+
+numeros = [10, 20, 30, 40, 50]
+minimo = min(numeros)
+maximo = max(numeros)
+normalizados = [(x - minimo) / (maximo - minimo) for x in numeros]
+
+print(normalizados)
 
 ### Exercício 8. Filtragem de Dados Faltantes
 # Objetivo:** Dada uma lista de dicionários representando dados de usuários, filtrar aqueles que têm um campo específico faltando
