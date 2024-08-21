b5acd0b372d758dd24ea0060ef8f55de7b0ef4d6
Gustavo Botelho Vieira <gu-botelho@hotmail.com>
2024-08-21 10:44:24 -0300

    adicionado exercicio 13

--- exercicios.py
+++ exercicios.py
@@ -157,18 +157,31 @@
 ### Exercício 12. Validação de Entrada
 # Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
 
-numero = int(input("Digite um número entre 1 e 10: "))
-while numero < 1 or numero > 10:
-    print("Número fora do intervalo!")
-    numero = int(input("Por favor, digite um número entre 1 e 10: "))
+# numero = int(input("Digite um número entre 1 e 10: "))
+# while numero < 1 or numero > 10:
+#     print("Número fora do intervalo!")
+#     numero = int(input("Por favor, digite um número entre 1 e 10: "))
 
-print("Número válido!")
+# print("Número válido!")
 
 ### Exercício 13. Consumo de API Simulado
 # Simular o consumo de uma API paginada, onde cada "página" de dados é processada em loop até que não haja mais páginas.
 
+pagina_atual = 1
+paginas_totais = 5  # Simulação, na prática, isso viria da API
+
+while pagina_atual <= paginas_totais:
+    print(f"Processando página {pagina_atual} de {paginas_totais}")
+    # Aqui iria o código para processar os dados da página
+    pagina_atual += 1
+
+print("Todas as páginas foram processadas.")
+
 ### Exercício 14. Tentativas de Conexão
 # Simular tentativas de reconexão a um serviço com um limite máximo de tentativas.
 
+
+
 ### Exercício 15. Processamento de Dados com Condição de Parada
 # Processar itens de uma lista até encontrar um valor específico que indica a parada.
+
